a08142274b4e8e12cde4572476377eb1f9392e45
Baker6Romeo <43591783+Baker6Romeo@users.noreply.github.com>
2022-10-19 07:24:41 -0700

    Challenge 1 Complete.

--- app.js
+++ app.js
@@ -15,19 +15,16 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.render("home");
+})
 
+// Depending on whether you're running local or hosted the port may be different
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Listening on port " + port + ".");
 });
