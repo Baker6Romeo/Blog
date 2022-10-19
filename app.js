4027c149d14b40e0323453c51231db484179b53f
Baker6Romeo <43591783+Baker6Romeo@users.noreply.github.com>
2022-10-19 07:31:15 -0700

    Challenge 2 attempt

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", {homeStartingContent: homeStartingContent});
 })
 
 // Depending on whether you're running local or hosted the port may be different
